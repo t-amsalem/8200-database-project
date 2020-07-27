6cf9a36b3ad8f6d35586f4cb7fbde499051f26b9
Your Name <you@example.com>
2020-07-27 12:39:39 +0300

    add function create_table to create table to our data base

--- db.py
+++ db.py
@@ -1,10 +1,13 @@
-# import db_api
-from .db_files import *
+import os
+import shelve
 
+import db_api
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
 
 @dataclass_json
 @dataclass
-class DBField:
+class DBField(db_api.DBField):
     def __init__(self, name, type):
         self.name = name
         self.type = type
@@ -12,7 +15,7 @@ class DBField:
 
 @dataclass_json
 @dataclass
-class SelectionCriteria:
+class SelectionCriteria(db_api.SelectionCriteria):
     def __init__(self, field_name, operator, value):
         self.field_name = field_name
         self.operator = operator
@@ -21,7 +24,7 @@ class SelectionCriteria:
 
 @dataclass_json
 @dataclass
-class DBTable:
+class DBTable(db_api.DBTable):
     def __init__(self, name, fields, key_field_name):
         self.name = name
         self.fields = fields
@@ -50,14 +53,82 @@ class DBTable:
 
         def create_index(self, field_to_index: str):
             raise NotImplementedError
+
+@dataclass_json
+@dataclass
+class DataBase:
+# Put here any instance information needed to support the API
+    def create_table(self, table_name: str, fields: List[DBField], key_field_name: str) -> DBTable:
+            raise NotImplementedError
+
+    def num_tables(self) -> int:
+            raise NotImplementedError
+
+    def get_table(self, table_name: str) -> DBTable:
+            raise NotImplementedError
+
+    def delete_table(self, table_name: str) -> None:
+            raise NotImplementedError
+
+    def get_tables_names(self) -> List[Any]:
+            raise NotImplementedError
+
+    def query_multiple_tables(self, tables: List[str], fields_and_values_list: List[List[SelectionCriteria]], fields_to_join_by: List[str]) -> List[Dict[str, Any]]:
+            raise NotImplementedError
+
+
+@dataclass_json
+@dataclass
 class DataBase(db_api.DataBase):
-    להריץ
-    טסט
-    מסוים:
-    py.test - k
-    test_name
-    להריץ
-    עם
-    מדידת
-    זמן:
-    pytest - -durations = 8
+# Put here any instance information needed to support the API
+    def create_table(self, table_name, fields, key_field_name):
+        value = {}
+        for field in fields:
+            value[field] = ''
+        path_file = os.path.join('db_files', table_name + '.db')
+        table_file = shelve.open(path_file)
+        table_file[key_field_name] = value
+        table_file.close()
+
+    def num_tables(self) -> int:
+        raise NotImplementedError
+
+    def get_table(self, table_name: str) -> DBTable:
+        raise NotImplementedError
+
+    def delete_table(self, table_name: str) -> None:
+        raise NotImplementedError
+
+    def get_tables_names(self) -> List[Any]:
+        raise NotImplementedError
+
+    def query_multiple_tables(self, tables: List[str], fields_and_values_list: List[List[SelectionCriteria]], fields_to_join_by: List[str]) -> List[Dict[str, Any]]:
+        raise NotImplementedError
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # להריץ
+    # טסט
+    # מסוים:
+    # py.test - k
+    # test_name
+    # להריץ
+    # עם
+    # מדידת
+    # זמן:
+    # pytest - -durations = 8
