820e9fa17b814cc88fca86472e87cde2cfb06f16
Your Name <you@example.com>
2020-07-27 13:35:45 +0300

    add function get table and update func create table to return DBTable

--- db.py
+++ db.py
@@ -5,7 +5,6 @@ import db_api
 from dataclasses import dataclass
 from dataclasses_json import dataclass_json
 
-table_list = []
 
 @dataclass_json
 @dataclass
@@ -31,73 +30,49 @@ class DBTable(db_api.DBTable):
         self.name = name
         self.fields = fields
         self.key_field_name = key_field_name
+        path_file = os.path.join('db_files', self.table_name + '.db')
+        table_file = shelve.open(path_file)
+        table_file.close()
 
-        def count(self):
-            raise NotImplementedError
+    def count(self):
+        raise NotImplementedError
 
-        def insert_record(self, values: Dict[str, Any]):
-            raise NotImplementedError
+    def insert_record(self, values: Dict[str, Any]):
+        raise NotImplementedError
 
-        def delete_record(self, key: Any):
-            raise NotImplementedError
+    def delete_record(self, key: Any):
+        raise NotImplementedError
 
-        def delete_records(self, criteria: List[SelectionCriteria]):
-            raise NotImplementedError
+    def delete_records(self, criteria: List[SelectionCriteria]):
+        raise NotImplementedError
 
-        def get_record(self, key: Any):
-            raise NotImplementedError
+    def get_record(self, key: Any):
+        raise NotImplementedError
 
-        def update_record(self, key: Any, values: Dict[str, Any]):
-            raise NotImplementedError
+    def update_record(self, key: Any, values: Dict[str, Any]):
+        raise NotImplementedError
 
-        def query_table(self, criteria: List[SelectionCriteria]):
-            raise NotImplementedError
+    def query_table(self, criteria: List[SelectionCriteria]):
+        raise NotImplementedError
 
-        def create_index(self, field_to_index: str):
-            raise NotImplementedError
-
-@dataclass_json
-@dataclass
-class DataBase:
-# Put here any instance information needed to support the API
-    def create_table(self, table_name: str, fields: List[DBField], key_field_name: str) -> DBTable:
-            raise NotImplementedError
-
-    def num_tables(self) -> int:
-            raise NotImplementedError
-
-    def get_table(self, table_name: str) -> DBTable:
-            raise NotImplementedError
-
-    def delete_table(self, table_name: str) -> None:
-            raise NotImplementedError
-
-    def get_tables_names(self) -> List[Any]:
-            raise NotImplementedError
-
-    def query_multiple_tables(self, tables: List[str], fields_and_values_list: List[List[SelectionCriteria]], fields_to_join_by: List[str]) -> List[Dict[str, Any]]:
-            raise NotImplementedError
+    def create_index(self, field_to_index: str):
+        raise NotImplementedError
 
 
 @dataclass_json
 @dataclass
 class DataBase(db_api.DataBase):
-# Put here any instance information needed to support the API
+    tables = {}
+
     def create_table(self, table_name, fields, key_field_name):
-        value = {}
-        for field in fields:
-            value[field] = ''
-        path_file = os.path.join('db_files', table_name + '.db')
-        table_file = shelve.open(path_file)
-        table_file[key_field_name] = value
-        table_file.close()
-        table_list.append(table_name)
+        self.tables[table_name] = DBTable(table_name, fields, key_field_name)
+        return self.tables[table_name]
 
     def num_tables(self):
-        return len(table_list)
+        return len(self.tables)
 
-    def get_table(self, table_name: str) -> DBTable:
-        raise NotImplementedError
+    def get_table(self, table_name):
+        return self.tables.get(table_name)
 
     def delete_table(self, table_name: str) -> None:
         raise NotImplementedError
